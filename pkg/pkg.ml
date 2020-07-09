2409341eb2d326de005e2380d473d4c4df506757
Sai Venkata Krishnan <saiganesha5.svkv@gmail.com>
2020-07-09 09:51:23 +0530

    Uncommented Logs_fmt. Commented *top* libraries

--- pkg/pkg.ml
+++ pkg/pkg.ml
@@ -15,11 +15,11 @@ let () =
   let fmt = Conf.value c fmt in
   let lwt = Conf.value c lwt in
   Ok [ Pkg.mllib "src/logs.mllib";
-       (* Pkg.mllib ~cond:fmt "src/logs_fmt.mllib"; *)
+       Pkg.mllib ~cond:fmt "src/logs_fmt.mllib";
        Pkg.mllib ~cond:jsoo "src/logs_browser.mllib";
        Pkg.mllib ~cond:cmdliner "src/logs_cli.mllib";
        Pkg.mllib ~cond:lwt "src/logs_lwt.mllib";
-       Pkg.mllib ~cond:fmt ~api:[] "src/logs_top.mllib";
+       (* Pkg.mllib ~cond:fmt ~api:[] "src/logs_top.mllib"; *)
        (* Pkg.lib "src/logs_top_init.ml"; *)
        (* Pkg.lib "src/logs_fmt_top_init.ml"; *)
        (* Pkg.doc "test/tool.ml"; *)
